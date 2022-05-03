74394a2f050de2f1cab6e91009125765f01afeb4
SebastianoMV <sebastianovisco@gmail.com>
2022-05-03 15:28:36 +0200

    cambio img con l'utilizzo delle frecce

--- js/script.js
+++ js/script.js
@@ -1,29 +1,55 @@
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    }
-];
 
-console.log(slides);
+const app = new Vue({
+    el: '#app',
+
+    data:{ 
+        slides: [
+        {
+            image: 'img/01.jpg',
+            title: 'Svezia',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        },
+        {
+            image: 'img/02.jpg',
+            title: 'Svizzera',
+            text: 'Lorem ipsum.',
+        },
+        {
+            image: 'img/03.jpg',
+            title: 'Gran Bretagna',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        },
+        {
+            image: 'img/04.jpg',
+            title: 'Germania',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+        },
+        {
+            image: 'img/05.jpg',
+            title: 'Paradise',
+            text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+        },
+        
+        ],
+        counterImg: 0,
+    },
+    methods: {
+       nextImg(){
+           this.counterImg++;
+           if(this.counterImg > this.slides.length -1){
+               this.counterImg = 0;
+           }
+           console.log(this.counterImg);
+       },
+       prevImg(){
+           this.counterImg--
+           if(this.counterImg < 0){
+            this.counterImg = this.slides.length -1;
+        }
+       }
+        
+    },
+
+})
+
+    CanvasRenderingContext2D.lenght
